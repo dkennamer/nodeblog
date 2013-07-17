aa881992ceda381c752ab5a763c9a612256bf14a
Dylan Kennamer <dylan.kennamer@gmail.com>
2013-07-17 23:57:53 +0900

    app.js modified

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 // Module dependencies.
 var express = require('express');
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
-var app = express.createServer();
+var app = module.exports = express.createServer();
 
 // Configuration
 app.configure( function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(require('stylus').middleware({src: __dirname + '/public'}));
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
+
+app.configure('development', function(){
+	app.use(express.errorHandler({dumpExceptions: true, showStack:true}));
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler());
+});
+
+var articleProvider = new ArticleProvider();
 
 // Routes
 app.get('/', function(req, res) {
-    res.send('Hello World');
-});
+    articleProvider.findAll(function(error, docs){
+	res.send(docs);
+	});
+})
 
 app.listen(3000);
